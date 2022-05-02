e948962e039960a6f8bdbf05c1648c3a8110146f
VošplouchTornádoCZgamingLP <kuba@septem.cz>
2022-05-02 11:50:13 +0200

    register03

--- register.js
+++ register.js
@@ -1,25 +1,111 @@
+$(document).ready(function() {
+    $('#contact_form').bootstrapValidator({
+        // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh',
+            valid: 'bi bi-123-ok',
+            invalid: "bi bi-123-remove",
+            validating:"bi bi-123-refresh"
+        },
+        fields: {
+            first_name: {
+                validators: {
+                        stringLength: {
+                        min: 2,
+                    },
+                        notEmpty: {
+                        message: 'Please enter your First Name'
+                    }
+                }
+            },
+             last_name: {
+                validators: {
+                     stringLength: {
+                        min: 2,
+                    },
+                    notEmpty: {
+                        message: 'Please enter your Last Name'
+                    }
+                }
+            },
+			 user_name: {
+                validators: {
+                     stringLength: {
+                        min: 8,
+                    },
+                    notEmpty: {
+                        message: 'Please enter your Username'
+                    }
+                }
+            },
+			 user_password: {
+                validators: {
+                     stringLength: {
+                        min: 8,
+                    },
+                    notEmpty: {
+                        message: 'Please enter your Password'
+                    }
+                }
+            },
+			confirm_password: {
+                validators: {
+                     stringLength: {
+                        min: 8,
+                    },
+                    notEmpty: {
+                        message: 'Please confirm your Password'
+                    }
+                }
+            },
+            email: {
+                validators: {
+                    notEmpty: {
+                        message: 'Please enter your Email Address'
+                    },
+                    emailAddress: {
+                        message: 'Please enter a valid Email Address'
+                    }
+                }
+            },
+            contact_no: {
+                validators: {
+                  stringLength: {
+                        min: 12, 
+                        max: 12,
+                    notEmpty: {
+                        message: 'Please enter your Contact No.'
+                     }
+                }
+            },
+			 department: {
+                validators: {
+                    notEmpty: {
+                        message: 'Please select your Department/Office'
+                    }
+                }
+            },
+                }
+            }
+        })
+        .on('success.form.bv', function(e) {
+            $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
+                $('#contact_form').data('bootstrapValidator').resetForm();
 
-var pass1E1 = document.querySelector("#password1")
-var pass2E1 = document.querySelector("#password1")
+            // Prevent form submission
+            e.preventDefault();
 
-function validatePasswords()
-    {
-    if (pass1El.value == pass2El.value)
-    {
-    pass1El.style.border = "2px solid green"  
-    pass2El.style.border = "2px solid green"
-        return true
-    }
-    else
-    {
-    pass1El.style.border = "2px solid red"
-    pass2El.style.border = "2px solid red"
-        return false
-    }}
-    pass1El.addEventListener("change", function(){
-    validatePasswords()
-    })
+            // Get the form instance
+            var $form = $(e.target);
 
-    pass2El.addEventListener("change", function(){
-    validatePasswords()
-    })
+            // Get the BootstrapValidator instance
+            var bv = $form.data('bootstrapValidator');
+
+            // Use Ajax to submit form data
+            $.post($form.attr('action'), $form.serialize(), function(result) {
+                console.log(result);
+            }, 'json');
+        });
+});
