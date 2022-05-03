53a63abe09100b19b8d40db2df3017f3e93c684f
VošplouchTornádoCZgamingLP <kuba@septem.cz>
2022-05-03 14:13:25 +0200

    opA

--- conn.php
+++ conn.php
@@ -1,7 +1,14 @@
 <?php
-$servername = "shop";
-$username = "shop";
-$password = "5itsE3ay8Qh8XlVR";
-$dbname = "shop";
-
+    $servername = "localhost";
+    $username = "shop";
+    $password = "5itsE3ay8Qh8XlVR";
+    $dbname = "shop";
+    
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    mysqli_query($conn,"SET NAMES utf8");
+    
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 ?>
